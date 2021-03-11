de8392b58d8f269cbfd5d09da023d9a5baceb5a3
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-03-11 13:46:01 +0500

    feat: add TravelAppContext
    added contextAPI to the app

--- src/App.js
+++ src/App.js
@@ -7,25 +7,26 @@ import MainPageHeader from "./components/Header/MainPageHeader";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import CardHeader from "./components/Header/CardHeader";
-
+import TravelAppContext from "./components/context/context";
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Switch>
-          <Route path="/country">
-            <CardHeader />
-            <CountryPage />
-          </Route>
-          <Route path="/">
-            <MainPageHeader />
-            <Main />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <TravelAppContext.Provider value={{ language: "UZ" }}>
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route path="/country">
+              <CardHeader />
+              <CountryPage />
+            </Route>
+            <Route path="/">
+              <MainPageHeader />
+              <Main />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </TravelAppContext.Provider>
   );
 }
-
 export default App;
